498ed4fe47fa7aafa90dd2c39efd5cf437a7717d
Hayes <hayes@Hayess-MacBook-Air.local>
2017-04-18 11:28:56 -0400

    this is my first commit

---
+++ python101.py
@@ -0,0 +1,8 @@
+# print "Hello, World!"
+# print two things on the same line
+print ("Hello, world", "Again")
+
+print """ this 
+will ignore line breaks
+until python finds 
+another three double quotes"""
