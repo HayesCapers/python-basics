3502a3dc8bc2472b0a4b6e52e6033d1b664cddd3
Hayes Capers <capersj5@yahoo.com>
2017-04-25 14:18:25 -0400

    init RPG using OOP

--- text_rpg/class_rpg.py
+++ text_rpg/class_rpg.py
@@ -1,38 +1,55 @@
 from hero import Hero
 from goblin import Goblin
+from Vampire import Vampire
 
-the_hero = Hero('Hayes')
-the_goblin = Goblin()
 
-while the_goblin.health > 0 and the_hero.health > 0:
-	print "You have %d health and %d power." % (the_hero.health, the_hero.power)
-	print "The goblin has %d health and %d power." % (the_goblin.health, the_goblin.power)
-	print "What do you want to do?"
-	print "1. fight goblin"
-	print "2. do nothing"
-	print "3. flee"
-	print "> ",
-	user_input = raw_input()
+hero_name = raw_input('What is your name? > ')
+the_hero = Hero(hero_name)
+# the_goblin = Goblin()
+monsters = []
+monsters.append(Goblin())
+monsters.append(Vampire())
 
-	if user_input == '1':
-		the_goblin.health -= the_hero.power
-		print "You have done %d damage to the goblin" % the_hero.power
-		if the_goblin.health <= 0:
-			print "you have defeated the goblin!"
-	elif user_input == '2':
-		pass
-	elif user_input == '3':
-		print 'Goobye coward.'
-		break
-	else:
-		print 'Ivalid input %s' % user_input
 
-	if (the_goblin.health > 0):
-		the_hero.health -= the_goblin.power
-		print 'The goblin hit you for %d damage' % the_goblin.power
-		if the_hero.health <= 0:
-			print "You are dead."
+print 'Welcome brave %s, you have encountered a goblin' % the_hero.name
+# print 'How many goblins are you willing to fight?'
+# number_of_enemies = 
 
-	if the_hero.health > 0 and the_hero.health < 5: 
-		print 'You have gone into a rage. Your power has increased!'
-		the_hero.power += 3
+for monster in monsters:
+	while monster.health > 0 and the_hero.is_alive():
+		print "%s has %d health and %d power." % (the_hero.name, the_hero.health, the_hero.power)
+		print "The %s has %d health and %d power." % (monster.name, monster.health, monster.power)
+		print "What do you want to do?"
+		print "1. fight goblin"
+		print "2. do nothing"
+		print "3. flee"
+		print "4. Drink liquor."
+		print "> ",
+		user_input = raw_input()
+
+		if user_input == '1':
+			monster.take_damage(the_hero.power)
+			print "%s does %d damage to the goblin" % (the_hero.name, the_hero.power)
+			if monster.health <= 0:
+				print "you have defeated the %s!" % monster.name
+				the_hero.xp += monster.xp_value
+				the_hero.check_level()
+		elif user_input == '2':
+			pass
+		elif user_input == '3':
+			print 'Goobye coward.'
+			break
+		elif user_input == '4':
+			the_hero.increase_health(20)
+		else:
+			print 'Ivalid input %s' % user_input
+
+		if (monster.health > 0):
+			the_hero.health -= monster.power
+			print 'The goblin hit you for %d damage' % monster.power
+			if the_hero.health <= 0:
+				print "You are dead."
+
+		if the_hero.is_alive() and the_hero.health < 5: 
+			print 'You have gone into a rage. Your power has increased!'
+			the_hero.power += 3
--- text_rpg/hero.py
+++ text_rpg/hero.py
@@ -3,9 +3,22 @@ class Hero(object):
 		self.name = name
 		self.health = 10
 		self.power = 5
+		self.max_health = self.health
+		self.xp = 0
+		self.level = 1
 
 	def cheer_hero(self):
 		print 'Fight hard, %s' % self.name
+
+	def check_level(self):
+		if self.xp > 3:
+			self.level = 2
+			self.level_up()
+	def level_up(self):
+		print "Thou hast leveled up %s! Thy hp is now %d and thy power is now %d" % (self.name, self.max_health, self.power)
+		self.max_health += 10
+		self.health = self.max_health
+		self.power += 5
 
 	#this class method returns a boolean. True is the hero is alive, False if the hero is dead
 	def is_alive(self):
@@ -13,3 +26,11 @@ class Hero(object):
 			return True
 		else:
 			return False
+
+	def attack(self, who):
+		who_to_attack.health -= self.power
+
+	def increase_health(self, amount):
+		self.health += amount
+		if self.health > self.max_health:
+			self.health = self.max_health
