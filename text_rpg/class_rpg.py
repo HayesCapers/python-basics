bf0f47c5b345962a9da8a327f6161c63d23cbef6
Hayes Capers <capersj5@yahoo.com>
2017-04-25 14:37:04 -0400

    unknown error

--- text_rpg/class_rpg.py
+++ text_rpg/class_rpg.py
@@ -1,20 +1,22 @@
 from hero import Hero
 from goblin import Goblin
 from Vampire import Vampire
-
+from random import randint
 
 hero_name = raw_input('What is your name? > ')
 the_hero = Hero(hero_name)
 # the_goblin = Goblin()
 monsters = []
-monsters.append(Goblin())
-monsters.append(Vampire())
-
+# monsters.append(Goblin())
+# monsters.append(Vampire())
+monster_selector = [Goblin(), Vampire()]
 
 print 'Welcome brave %s, you have encountered a goblin' % the_hero.name
-# print 'How many goblins are you willing to fight?'
-# number_of_enemies = 
-
+print 'How many enemies are you willing to fight?'
+number_of_enemies = int(raw_input('> '))
+for i in range(0, number_of_enemies):
+	monsters.append(monster_selector[randint(0,1)])
+print monsters
 for monster in monsters:
 	while monster.health > 0 and the_hero.is_alive():
 		print "%s has %d health and %d power." % (the_hero.name, the_hero.health, the_hero.power)
@@ -29,7 +31,7 @@ for monster in monsters:
 
 		if user_input == '1':
 			monster.take_damage(the_hero.power)
-			print "%s does %d damage to the goblin" % (the_hero.name, the_hero.power)
+			print "%s does %d damage to the %s" % (the_hero.name, the_hero.power, monster.name)
 			if monster.health <= 0:
 				print "you have defeated the %s!" % monster.name
 				the_hero.xp += monster.xp_value
